d2db209bca6b9001ae2f7061cc88ff97afc94a66
Jørgen Finsveen <joergen.finsveen@gmail.com>
2023-01-28 12:28:41 +0100

    initialized new spring boot project

---
+++ backend/src/main/java/no/ntnu/mocha/MochaApplication.java
@@ -0,0 +1,13 @@
+package no.ntnu.mocha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MochaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MochaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/no/ntnu/mocha/MochaApplicationTests.java
@@ -0,0 +1,13 @@
+package no.ntnu.mocha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MochaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
