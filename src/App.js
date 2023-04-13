75346e640122a65c314c2f6c02bc053257a51199
Petter Edvardsen <Petter-edvardsen@hotmail.com>
2023-04-13 16:18:54 +0200

    Responsive layout for the navigation bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import { NavBar } from "./Navigation/NavBar";
 import { Footer } from "./Footer/Footer";
-<<<<<<< HEAD
 import { BrowserRouter as Router } from "react-router-dom";
 import { MainSection } from "./MainSection";
-=======
-import { Carousel } from "./Testimonials/Carousel";
-import { Product } from "./Products/Product";
-import { ProductPage } from "./ProductPage/ProductPage";
->>>>>>> 4a461253c541bf9149ddeae34c41d129405f2d6a
 
 /**
  * Component representing the main page
--- src/Navigation/NavBar.js
+++ src/Navigation/NavBar.js
@@ -1,59 +1,61 @@
 import { useState, useEffect } from "react";
-import "./Navbar.css"
+import "./Navbar.css";
 import { ActiveLink } from "./ActiveLink";
 
 /**
  * A collection of links to navigate the webpage
- * 
+ *
  * @return {JSX.Element}
  * @constructor
  */
 
-export function NavBar(){
-    
-    const [itemsInCart,setNumberOfItemsInCart] = useState(2);
+export function NavBar() {
+  const [itemsInCart, setNumberOfItemsInCart] = useState(2);
 
-    useEffect(loadItemsInCart);
+  useEffect(loadItemsInCart);
 
-    return( 
-        <nav>
-            <logo class="fa fa-coffee fa-5x" aria-hidden="true" id="logo"></logo>
-            <ActiveLink to="/">
-                <h4>HOME</h4>
-            </ActiveLink>
-            <ActiveLink to="/products">
-                <h4>PRODUCTS</h4>
-            </ActiveLink>
-            <ActiveLink to="/about">
-                <h4>ABOUT US</h4>
-            </ActiveLink>
-            <ActiveLink to="/contact">
-                <h4>CONTACT</h4>
-            </ActiveLink>
-            <container class="cart-container">
-                <ActiveLink to="/cart">
-                    <i class="fa fa-shopping-cart fa-5x" aria-hidden="true" id="cart-icon"></i>
-                </ActiveLink>
-                <counter>{itemsInCart}</counter>
-            </container>
-        </nav>
-    );
+  return (
+    <nav>
+      <i class="fa fa-bars fa-4x" aria-hidden="true" id="bars-icon"></i>
+      <logo class="fa fa-coffee fa-5x" aria-hidden="true" id="logo"></logo>
+      <ActiveLink to="/">
+        <h4>HOME</h4>
+      </ActiveLink>
+      <ActiveLink to="/products">
+        <h4>PRODUCTS</h4>
+      </ActiveLink>
+      <ActiveLink to="/about">
+        <h4>ABOUT US</h4>
+      </ActiveLink>
+      <ActiveLink to="/contact">
+        <h4>CONTACT</h4>
+      </ActiveLink>
+      <container className="user-cart-container">
+        <i class="fa fa-user fa-5x" aria-hidden="true" id="user-icon"></i>
+        <container class="cart-container">
+          <ActiveLink to="/cart">
+            <i
+              class="fa fa-shopping-cart fa-5x"
+              aria-hidden="true"
+              id="cart-icon"
+            ></i>
+          </ActiveLink>
+          <counter>{itemsInCart}</counter>
+        </container>
+      </container>
+    </nav>
+  );
 
+  /**
+   *
+   */
+  function loadItemsInCart() {
+    // Send request to API for the number of items in the users cart
+    // E.g queryCartItems().length
 
-    /**
-     * 
-     */
-    function loadItemsInCart(){
+    // Dummy number to showcase functionality
+    const numberOfItems = 1;
 
-        // Send request to API for the number of items in the users cart
-        // E.g queryCartItems().length
-
-        // Dummy number to showcase functionality
-        const numberOfItems = 1;
-
-        setNumberOfItemsInCart(numberOfItems)
-    }
-
+    setNumberOfItemsInCart(numberOfItems);
+  }
 }
-
-
