d95842b8612ac32c8ed7ab56adfc11101d4cb58c
Petter Edvardsen <Petter-edvardsen@hotmail.com>
2023-05-27 19:38:30 +0200

    Integrating development hero-section

---
+++ frontend/src/components/Showcase/Showcase.js
@@ -0,0 +1,28 @@
+import "./Showcase.css";
+import { ShowcaseCard } from "./ShowcaseCard";
+
+const jsonTestData = {
+  id: "3",
+  name: "Black Coffe",
+  price: "122",
+  description: "Brazilian coffee is sealed for freshness...",
+};
+
+/**
+ * A section of showcased products
+ *
+ * @return {JSX.Element}
+ * @constructor
+ */
+export function Showcase() {
+  return (
+    <container className="product-section">
+      <h2>New Releases</h2>
+      <container className="product-container">
+        <ShowcaseCard props={jsonTestData} />
+        <ShowcaseCard props={jsonTestData} />
+        <ShowcaseCard props={jsonTestData} />
+      </container>
+    </container>
+  );
+}
