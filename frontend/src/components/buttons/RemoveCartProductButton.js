8831484105f6fe4e3aceabb322e8f87d2fe2fa51
Oskar <oskarlo@stud.ntnu.no>
2023-05-28 19:00:00 +0200

    Added react components and cart page

---
+++ frontend/src/components/buttons/RemoveCartProductButton.js
@@ -0,0 +1,11 @@
+import "./RemoveCartProductButton.css"
+/**
+ * A button that removes a product from a shopping cart.
+ * 
+ * @returns {JSX.Element} button removing cart product
+ */
+export function RemoveCartProductButton() {
+    return (
+        <button className="removeCartProductButton">X</button>
+    );
+}
