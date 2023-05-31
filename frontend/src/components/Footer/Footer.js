b22aca8247d6b9e18f1a05283ff95dcbe0fde340
Oskar <oskarlo@stud.ntnu.no>
2023-05-31 11:18:21 +0200

    Fixed footer

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -16,61 +16,10 @@ import "./Footer.css";
 export function Footer() {
   return (
     <footer>
-      <Accordion className="accordion">
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="about-us-content"
-          id="about-us-header"
-        >
-          <Typography sx={{ fontSize: 20 }}>About us</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography sx={{ fontSize: 16 }}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-
-      <Accordion className="accordion">
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="contact-us-content"
-          id="contact-us-header"
-        >
-          <Typography sx={{ fontSize: 20 }}>Contact us</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography sx={{ fontSize: 16 }}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-
-      <Accordion className="accordion">
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="info-content"
-          id="info-header"
-        >
-          <Typography sx={{ fontSize: 20 }}>Info</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography sx={{ fontSize: 16 }}>
-            This website is a result of a university group project, performed in
-            the course IDATA2301 Web technologies, at NTNU. All the information
-            provided here is a result of imagination. Any resemblance with real
-            companies or products is a coincidence.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-
-      <div id="footer-icons">
-        <i className="fa fa-facebook fa-5x" aria-hidden="true"></i>
-        <i className="fa fa-instagram fa-5x" aria-hidden="true"></i>
-        <i className="fa fa-twitter fa-5x" aria-hidden="true"></i>
-      </div>
+      This website is a result of a university group project, performed in
+      the course IDATA2301 Web technologies, at NTNU. All the information
+      provided here is a result of imagination. Any resemblance with real
+      companies or products is a coincidence.
     </footer>
   );
 }
