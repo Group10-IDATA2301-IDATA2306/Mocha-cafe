9a924609c37d5ffa3d07ea1ea8c38d9f5ff8b1a8
Oskar <oskarlo@stud.ntnu.no>
2023-05-31 07:24:50 +0200

    Minor changes

--- frontend/src/components/buttons/FormButton.js
+++ frontend/src/components/buttons/FormButton.js
@@ -1,4 +1,6 @@
+import { useContext } from "react";
 import "./FormButton.css"
+import { CartContext } from "../../context/CartContext";
 
 /**
  * A button for forms.
@@ -7,7 +9,22 @@ import "./FormButton.css"
  * @returns {JSX.Element} a button with the desired text
  */
 export function FormButton(props) {
+    const { cartItems } = useContext(CartContext);
+
+    const submit = () => {
+        const submitList = cartItems.map((item) => {
+            return {
+                productId: item.id,
+                amount: item.quantity,
+            };
+        });
+        console.log("success");
+    }
+
     return (
-        <button className="formButton" id={props.id}>{props.text}</button>
+        <button className="formButton"
+            id={props.id}
+            onClick={submit}
+        >{props.text}</button>
     );
 }
